96be3aa98ae0108cd4204feb4de856807b3fff11
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-18 15:02:34 -0800

    SOS

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
+  superbowlWin = (record) => {
+   let result = record.find( record => record.result === 'W')
+  return !!result ? result.year : undefined
+ }
+
+// superbowlWin = (record) => {
+//   let result = record.find( record => record.result === "W" )
+//   return !!result ? result.year : undefined
+// }
